8081d12c64dce4044d851800e01d28c8181ec6bc
dekeyrej <joedekeyrel@gmail.com>
2026-04-15 13:37:41 -0400

    Infer architecture (amd64 or x86_64) from distrobution

--- build_all.sh
+++ build_all.sh
@@ -2,6 +2,10 @@
 set -euo pipefail
 
 # override with PVENODES="bluep bluep02 bluep03"
+home_dir=$(eval echo ~${SUDO_USER:-$USER})
+log_file="$home_dir/repos/custom-container-images/container_log.txt"
+echo "Home directory: $home_dir"
+
 default_nodes="iluvatar"
 pvenodes_str="${PVENODES:-$default_nodes}"
 read -r -a pvenodes <<< "$pvenodes_str"
@@ -9,7 +13,7 @@ read -r -a pvenodes <<< "$pvenodes_str"
 # override with PHYSICAL_IMAGE_PATH="/mnt/bucket/template/cache/"
 ct_image_dir=${PHYSICAL_IMAGE_PATH:-"/mnt/ssd_backup/template/cache/"}
 
-echo "Custom Container Build Started - $(date)" > container_log.txt
+echo "Custom Container Build Started - $(date)" > $log_file
 
 amazon_tarball() {
   local arch=$1
@@ -33,8 +37,13 @@ amazon_tarball() {
 build_image() {
   local distro=$1
   local release=$2
-  local arch=$3
   local extra_opts=()
+
+  if [[ $distro == "amazonlinux" || $distro == "centos" || $distro == "rockylinux" ]]; then
+    local arch="x86_64"
+  else # default to amd64 for ubuntu/debian
+    local arch="amd64"
+  fi
 
   if [[ ! -f authorized_keys ]]; then
     echo "authorized_keys not found!"
@@ -56,7 +65,7 @@ build_image() {
 
   if [[ $? -ne 0 ]]; then
     echo "❌ Build failed for $distro:$release"
-    echo "❌ Build failed for $distro:$release at $(date)" >> container_log.txt
+    echo "❌ Build failed for $distro:$release at $(date)" >> $log_file
     exit 1
   fi
 
@@ -66,7 +75,7 @@ build_image() {
   mv "$distro/$release/rootfs.tar.xz" "$distro-$release-latest-custom.tar.xz"
   rm -rf "$distro"
   echo "✅ Finished $distro:$release"
-  echo "✅ Finished $distro:$release at $(date)" >> container_log.txt
+  echo "✅ Finished $distro:$release at $(date)" >> $log_file
   for h in "${pvenodes[@]}"; do
       scp -i /home/ubuntu/.ssh/id_rsa \
           "$distro-$release-latest-custom.tar.xz" \
@@ -75,25 +84,25 @@ build_image() {
 }
 
 # Ubuntu builds - creates ubuntu user
-build_image ubuntu jammy amd64 
-build_image ubuntu noble amd64
-# build_image ubuntu questing amd64 # (25.10) deprecated - switched to resolute (26.04)
-build_image ubuntu resolute amd64
+# build_image ubuntu jammy
+build_image ubuntu noble
+# build_image ubuntu questing # (25.10) deprecated - switched to resolute (26.04)
+build_image ubuntu resolute
 
 # Debian builds - creates debian user
-build_image debian bookworm amd64
-build_image debian trixie amd64
-build_image debian forky amd64
+# build_image debian bookworm
+build_image debian trixie
+build_image debian forky
 
 # Rocky Linux builds - creates cloud-user
-build_image rockylinux  9 x86_64
-build_image rockylinux 10 x86_64
+# build_image rockylinux  9
+build_image rockylinux 10
 
 # CentOS builds - creates cloud-user # deprecated - switched to Rockylinux
-# build_image centos  9-Stream x86_64
-# build_image centos 10-Stream x86_64
+# build_image centos  9-Stream
+# build_image centos 10-Stream
 
 # Amazon Linux 2023 build - creates ecs-user
-build_image amazonlinux 2023 x86_64
+# build_image amazonlinux 2023
 
 echo "Custom Container Build Complete - $(date)" >> container_log.txt
